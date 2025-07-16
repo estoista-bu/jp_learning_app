60ad4ef70c145f4a8e03da4ea873004af88dc101
GREMYCO ESTOISTA <estoista_g@bu.glue-si.com>
2025-07-16 06:07:19 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+        config.externals.push('wanakana');
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
